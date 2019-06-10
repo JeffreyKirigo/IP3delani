55dd1472a4d7381618e2e863d8228201e7666e38
jeffrey muriithi <jeffreyem99@gmail.com>
2019-06-10 20:03:31 +0300

    Hover on enter working

--- js/scripts.js
+++ js/scripts.js
@@ -34,10 +34,56 @@ $(document).ready(function () {
 
 
 $(document).ready(function () {
-$(".wk2").mouseenter(function () { 
-  $(".p2").show();  
-});
-$(".wk2").mouseleave(function () { 
-  $(".p2").hide();  
-});
+  $(".wk1").mouseenter(function () {
+    $(".p1").show();
+  });
+  $(".wk1").mouseleave(function () {
+    $(".p1").hide();
+  });
+  $(".wk2").mouseenter(function () {
+    $(".p2").show();
+  });
+  $(".wk2").mouseleave(function () {
+    $(".p2").hide();
+  });
+  $(".wk3").mouseenter(function () {
+    $(".p3").show();
+  });
+  $(".wk3").mouseleave(function () {
+    $(".p3").hide();
+  });
+  $(".wk4").mouseenter(function () {
+    $(".p4").show();
+  })
+  $(".wk4").mouseleave(function () {
+    $(".p4").hide();
+  });
+  $(".wk5").mouseenter(function () {
+    $(".p5").show();
+  });
+  $(".wk5").mouseleave(function () {
+    $(".p5").hide();
+  });
+  $(".wk6").mouseenter(function () {
+    $(".p6").show();
+  });
+  $(".wk6").mouseleave(function () {
+    $(".p6").hide();
+  });
+  $(".wk7").mouseenter(function () {
+    $(".p7").show();
+  });
+  $(".wk7").mouseleave(function () {
+    $(".p7").hide();
+  });
+  $(".wk8").mouseleave(function () {
+    $(".p8").hide();
+  });
+  $(".wk8").mouseenter(function () {
+    $(".p8").show();
+  });
+  $("#submit").click(function (e) { 
+    alert("Your message has been successfully received.")
+    
+  });
 });
